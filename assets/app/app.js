7c0b788dd2041d2607e7210688ed746098bea556
Cliff <cliff.patterson2@gmail.com>
2017-12-13 12:01:38 -0500

    Creates initial card data and mark card functionality on DOM
    Initial 'Deck' is loaded into local storage, so that it can be compared
    or added too in future.

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,5 +1,5 @@
 
-var gameStack = [
+var initCardStack = [
     {
     front: 'CARD FRONT', back: 'CARD BACK'
     }, 
@@ -7,21 +7,63 @@ var gameStack = [
     front: 'CARD FRONT 2', back: 'CARD BACK 2'}
 ]
 
+var currentGameStack = []
+
+/*
+ *  Init Game Cards cards
+ *  
+ *  Should populate game with data, and allow
+ *  for future stacks.  
+ *
+ */
+
+
+function checkLocalCardStack(){
+ if(localStorage.getItem('initCardStack')){
+   return true
+ } else {
+   return false
+ }
+}
+
+function initGameStack( checkedStack ){
+  if( checkedStack == true ){
+    return localStorage.getItem( 'initCardStack' )
+  } else {
+    return localStorage.setItem( 'initCardStack', JSON.stringify(initCardStack))
+  }
+}
+
 function loadCards( storage, array ){
   storage = JSON.parse( storage )
-  array = storage
+  storage.forEach(function(element) {
+    array.push(element)
+  }) 
   return array
 }
+
+/*
+ * Card getting and Display 
+ *
+ *
+ *
+ */
 
 function getTopCard( cardStack ){
   topCard = cardStack.shift()
   return topCard
 }
 
-function displayTopCard( topCard ){
+function displayCard( topCard ){
   $('figure#front').contents().replaceWith(topCard.front)
   $('figure#back').contents().replaceWith(topCard.back)
 
 } 
 
-
+function markCard( assesment ){
+  if ( assesment === 'correct'){
+    $('.card').attr('data-mark', 'correct')
+  } else {
+    $('.card').attr('data-mark', 'incorrect')
+  }
+}
